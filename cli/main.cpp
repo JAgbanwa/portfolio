3e5d9ff773fa13287bfae0a8da863802f7da871c
Jamal Agbanwa <jamal.agbanwa@student.uantwerpen.be>
2024-12-29 21:11:10 +0100

    downloaded starter code for school game

---
+++ cli/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace;
+
+int main(){
+    cout <<"Hello World!"<<endl;
+    cout<<"This is my work"<<endl;
+
+    return 0;
+}
---
+++ games/start_code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  !!! Dit bestand moet je in principe NIET wijzigen !!!
+//  !!!       Enkel de filename mag je aanpassen      !!!
+//
+
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(700, 700), "Game");
+
+    /*Instance of a class*/
+    Game game = Game(&window);
+    game.loadMap("resources/map.txt");
+    game.update();
+    return 0;
+}
+
